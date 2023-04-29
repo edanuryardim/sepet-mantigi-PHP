880323405ce6a2d64bcfd4707e47d98f6de50550
Eda Nur Yardım <94635163+edanuryardim@users.noreply.github.com>
2023-04-29 18:04:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -41,7 +41,40 @@
 
 </div>
 </div>
-<script src="script.js"></script>
+<script>
+  $(".addToCartBtn").click(function(){
+        
+        var url = "http://localhost/s/cart_db.php";
+        var data = {
+           p : "addToCart",
+           product_id: $(this).attr("product-id")
+           
+        } 
+  
+      $.post(url , data , function(response){
+        
+        $(".cart-count").text(response);
+      })
+      })
+  
+      
+      $(".removeFromCartBtn").click(function(){
+  
+          var url = "http://localhost/s/cart_db.php";
+          var data = {
+             p : "removeFromCart",
+             product_id: $(this).attr("product-id")
+             
+          } 
+  
+  
+        $.post(url , data , function(response){
+          
+          window.location.reload();
+        })
+      })
+      </script>
+
 
 
 
